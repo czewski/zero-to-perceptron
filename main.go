8e4e5b33ea4f902bd09df8d2cdcec61b50d1e490
eduardo <eduardohb10@hotmail.com>
2024-04-14 19:29:01 -0300

    fix: progress in NN

--- main.go
+++ main.go
@@ -2,14 +2,18 @@ package main
 
 import (
 	"perceptron/pkg/biases"
+	"perceptron/pkg/data"
 )
 
 func main() {
 	//https://pub.towardsai.net/the-multilayer-perceptron-built-and-implemented-from-scratch-70d6b30f1964
 
 	//Generate data
+	inputMatrix := data.GenerateData()
+	groundTruthLabels := data.GenerateLabels()
+	_, _ = inputMatrix, groundTruthLabels
 
-	//Generate NN
+	//Generate NN -- This needs to make more sense still
 	input := biases.Layer{}
 	hidden := biases.Layer{}
 	output := biases.Layer{}
@@ -20,10 +24,21 @@ func main() {
 		Output: []biases.Layer{output},
 	}
 
+	//Parameters Input > Hidden (2x3) = 6
 	parametersInputHidden := make([][]float64, NN.LenInput())
 	for i := 0; i < NN.LenInput(); i++ {
 		parametersInputHidden[i] = make([]float64, NN.LenHidden())
 	}
+
+	//Parameters Hidden > Output (3x1) = 3
+	parametersHiddenOutput := make([][]float64, NN.LenHidden())
+	for i := 0; i < NN.LenInput(); i++ {
+		parametersHiddenOutput[i] = make([]float64, NN.LenOutput())
+	}
+
+	//Initialize parameters with gaussian distribution
+
+	//Linear transformation from input to hidden
 
 	//Train
 
