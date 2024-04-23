31fd47336d9702e857b3c3c3c337bc48c675fd31
eduardo <eduardohb10@hotmail.com>
2024-04-23 13:39:54 -0300

    feat: propag/backpropag

--- pkg/network/backpropagation.go
+++ pkg/network/backpropagation.go
@@ -1,1 +1,16 @@
 package network
+
+import "perceptron/pkg/maths"
+
+func Backpropagation(zMatrixOne, hMatrixOne, prediction, groundTruthLabels, inputMatrix, weightMatrixTwo [][]float64) (partialDerivativeOne, partialDerivativeTwo [][]float64) {
+	//∂loss/∂p *∂p/∂zh * ∂zh/∂wh
+	delOne := maths.SubtractElementsMatrices(prediction, groundTruthLabels)
+	partialDerivativeOne = maths.MultiplyMatrices(maths.Transpose(hMatrixOne), delOne)
+
+	//∂loss/∂p * ∂p/∂zh * ∂zh/∂h * ∂h/∂z * ∂z/∂w
+	delTwo := maths.MultiplyElementWiseMatrices(delOne, maths.Transpose(weightMatrixTwo))
+	delThree := maths.MultiplyElementWiseMatrices(delTwo, SigmoidDerivative(zMatrixOne))
+	partialDerivativeTwo = maths.MultiplyMatrices(maths.Transpose(inputMatrix), delThree)
+
+	return partialDerivativeOne, partialDerivativeTwo
+}
--- pkg/network/propagation.go
+++ pkg/network/propagation.go
@@ -1,1 +1,21 @@
 package network
+
+import (
+	"perceptron/pkg/maths"
+)
+
+func Propagation(inputMatrix [][]float64, weightMatrixOne [][]float64, weightMatrixTwo [][]float64) (zMatrixOne, hMatrixOne, prediction [][]float64) {
+	//Z1 MATRIX: Multiply Input x InputHidden ---- row = each of the 4 training examples | column = z node
+	zMatrixOne = maths.MultiplyMatrices(inputMatrix, weightMatrixOne)
+
+	//Activation function (at hidden layer) ---- apply sigmoid to each element in z matrix
+	hMatrixOne = SigmoidMatrix(zMatrixOne)
+
+	//Get Z2 Matrix for Hidden Output
+	zMatrixTwo := maths.MultiplyMatrices(hMatrixOne, weightMatrixTwo)
+
+	//Activation for Hidden Output == Prediction
+	prediction = SigmoidMatrix(zMatrixTwo)
+
+	return zMatrixOne, hMatrixOne, prediction
+}
