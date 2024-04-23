e80513d6e03e906bec7548e423729ca0dc8058e2
eduardo <eduardohb10@hotmail.com>
2024-04-23 13:39:04 -0300

    feat: matrix operations

--- pkg/maths/matrix.go
+++ pkg/maths/matrix.go
@@ -30,6 +30,82 @@ func MultiplyMatrices(A, B [][]float64) [][]float64 {
 	return C
 }
 
+func SubtractElementsMatrices(A, B [][]float64) [][]float64 {
+	rowsA, colsB := len(A), len(B[0])
+
+	//TODO: check for same dimension
+
+	// Initialize result matrix C
+	C := make([][]float64, rowsA)
+	for i := range C {
+		C[i] = make([]float64, colsB)
+	}
+
+	for i := 0; i < rowsA; i++ {
+		for j := 0; j < colsB; j++ {
+			C[i][j] = A[i][j] - B[i][j]
+		}
+	}
+
+	return C
+}
+
+func MultiplyElementWiseMatrices(A, B [][]float64) [][]float64 {
+	rowsA, colsB := len(A), len(B[0])
+
+	//TODO: check for same dimension
+
+	// Initialize result matrix C
+	C := make([][]float64, rowsA)
+	for i := range C {
+		C[i] = make([]float64, colsB)
+	}
+
+	for i := 0; i < rowsA; i++ {
+		for j := 0; j < colsB; j++ {
+			C[i][j] = A[i][j] * B[i][j]
+		}
+	}
+
+	return C
+}
+
+func MultiplyValueToMatrices(A float64, B [][]float64) [][]float64 {
+	rowsB, colsB := len(B), len(B[0])
+
+	// Initialize result matrix C
+	C := make([][]float64, rowsB)
+	for i := range C {
+		C[i] = make([]float64, colsB)
+	}
+
+	for i := 0; i < rowsB; i++ {
+		for j := 0; j < colsB; j++ {
+			C[i][j] = A * B[i][j]
+		}
+	}
+
+	return C
+}
+
+// https://gist.github.com/tanaikech/5cb41424ff8be0fdf19e78d375b6adb8
+func Transpose(slice [][]float64) [][]float64 {
+	xl := len(slice[0])
+	yl := len(slice)
+
+	result := make([][]float64, xl)
+	for i := range result {
+		result[i] = make([]float64, yl)
+	}
+
+	for i := 0; i < xl; i++ {
+		for j := 0; j < yl; j++ {
+			result[i][j] = slice[j][i]
+		}
+	}
+	return result
+}
+
 func PrintMatrix(matrix [][]float64) {
 	for _, row := range matrix {
 		fmt.Println(row)
