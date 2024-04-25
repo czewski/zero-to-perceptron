b06e76ef15858bd4238e0c6db25654225b677afb
eduardo <eduardohb10@hotmail.com>
2024-04-24 21:36:59 -0300

    feat: initialize random weight matrix

--- pkg/network/parameters.go
+++ pkg/network/parameters.go
@@ -1,20 +1,32 @@
 package network
 
-func InitializeParameters(NN Network) ([][]float64, [][]float64) {
+import "math/rand"
 
+func InitializeParameters(NN Network) ([][]float64, [][]float64) {
 	//Parameters Input > Hidden (2x3) = 6
 	weightMatrixOne := make([][]float64, NN.LenInput())
 	for i := 0; i < NN.LenInput(); i++ {
 		weightMatrixOne[i] = make([]float64, NN.LenHidden())
 	}
 
-	//Parameters Hidden > Output (3x1) = 3 // 3x1 or 1x3 to make it work?
+	//Parameters Hidden > Output (3x1) = 3
 	weightMatrixTwo := make([][]float64, NN.LenHidden())
 	for i := 0; i < NN.LenHidden(); i++ {
 		weightMatrixTwo[i] = make([]float64, NN.LenOutput())
 	}
 
-	//TODO: Initialize parameters with gaussian distribution //or random
+	//Initialize random values
+	weightMatrixOne = initializeRandomValues(weightMatrixOne)
+	weightMatrixTwo = initializeRandomValues(weightMatrixTwo)
 
 	return weightMatrixOne, weightMatrixTwo
+}
+
+func initializeRandomValues(slice [][]float64) [][]float64 {
+	for i := range slice {
+		for j := range slice[i] {
+			slice[i][j] = rand.NormFloat64()
+		}
+	}
+	return slice
 }
