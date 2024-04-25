9d9f060a796bc116d1bd90a27f2a2935e93c4efa
eduardo <eduardohb10@hotmail.com>
2024-04-24 21:37:57 -0300

    fix: renamed MultiplyMatrices to DotMatrices

--- pkg/network/backpropagation.go
+++ pkg/network/backpropagation.go
@@ -1,16 +1,21 @@
 package network
 
-import "perceptron/pkg/maths"
+import (
+	"perceptron/pkg/maths"
+)
 
 func Backpropagation(zMatrixOne, hMatrixOne, prediction, groundTruthLabels, inputMatrix, weightMatrixTwo [][]float64) (partialDerivativeOne, partialDerivativeTwo [][]float64) {
 	//∂loss/∂p *∂p/∂zh * ∂zh/∂wh
 	delOne := maths.SubtractElementsMatrices(prediction, groundTruthLabels)
-	partialDerivativeOne = maths.MultiplyMatrices(maths.Transpose(hMatrixOne), delOne)
+
+	partialDerivativeTwo = maths.DotMatrices(maths.Transpose(hMatrixOne), delOne)
 
 	//∂loss/∂p * ∂p/∂zh * ∂zh/∂h * ∂h/∂z * ∂z/∂w
 	delTwo := maths.MultiplyElementWiseMatrices(delOne, maths.Transpose(weightMatrixTwo))
+
 	delThree := maths.MultiplyElementWiseMatrices(delTwo, SigmoidDerivative(zMatrixOne))
-	partialDerivativeTwo = maths.MultiplyMatrices(maths.Transpose(inputMatrix), delThree)
+
+	partialDerivativeOne = maths.DotMatrices(maths.Transpose(inputMatrix), delThree)
 
 	return partialDerivativeOne, partialDerivativeTwo
 }
