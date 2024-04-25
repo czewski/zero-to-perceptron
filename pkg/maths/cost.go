cfd82a96f6576a0c6b618c6a9fc9a35915483448
eduardo <eduardohb10@hotmail.com>
2024-04-24 21:36:12 -0300

    fix: cost function to work with [][]float64

--- pkg/maths/cost.go
+++ pkg/maths/cost.go
@@ -9,13 +9,24 @@ func binaryCrossEntropy(predicted float64, groundTruth float64) (loss float64) {
 	return loss
 }
 
-func CostBCE(predicted []float64, groundTruth []float64) (meanLoss float64) {
-
+func CostBCE(predicted [][]float64, groundTruth [][]float64) (meanLoss float64) {
 	summedLoss := 0.0
 
-	for indexTrainingExample := range predicted {
-		summedLoss += binaryCrossEntropy(predicted[indexTrainingExample], groundTruth[indexTrainingExample])
+	rowsA, colsB := len(predicted), len(predicted[0])
+	C := make([][]float64, rowsA)
+	for i := range C {
+		C[i] = make([]float64, colsB)
 	}
+
+	for i := 0; i < rowsA; i++ {
+		for j := 0; j < colsB; j++ {
+			summedLoss += binaryCrossEntropy(predicted[i][j], groundTruth[i][j])
+		}
+	}
+
+	// for indexTrainingExample := range predicted {
+	// 	summedLoss += binaryCrossEntropy(predicted[indexTrainingExample], groundTruth[indexTrainingExample])
+	// }
 
 	meanLoss = summedLoss / float64(len(predicted))
 	return meanLoss
