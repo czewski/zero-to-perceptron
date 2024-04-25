a565e88f4b2f8fccf850c40c5d18b81e101cc09e
eduardo <eduardohb10@hotmail.com>
2024-04-24 21:38:09 -0300

    feat: working network

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"perceptron/pkg/data"
 	"perceptron/pkg/maths"
 	"perceptron/pkg/network"
@@ -9,7 +10,7 @@ import (
 // ANN definitions
 const (
 	learningRate = 0.1
-	iterations   = 100000
+	iterations   = 10000000
 	inputLayer   = 2
 	hiddenLayer  = 3
 	outputLayer  = 1
@@ -22,24 +23,37 @@ func main() {
 
 	//Initialize Parameters
 	weightMatrixOne, weightMatrixTwo := network.InitializeParameters(NN)
-	var cost [][]float64
+	var costs []float64
 	var predictions [][][]float64
+	var zMatrixOne, hMatrixOne, prediction, partialDerivativeOne, partialDerivativeTwo [][]float64
 
 	//Train
 	for epoch := 0; epoch < iterations; epoch++ {
-		zMatrixOne, hMatrixOne, prediction := network.Propagation(inputMatrix, weightMatrixOne, weightMatrixTwo)
-		partialDerivativeOne, partialDerivativeTwo := network.Backpropagation(zMatrixOne, hMatrixOne, prediction, groundTruthLabels, inputMatrix, weightMatrixTwo)
+		zMatrixOne, hMatrixOne, prediction = network.Propagation(inputMatrix, weightMatrixOne, weightMatrixTwo)
+
+		partialDerivativeOne, partialDerivativeTwo = network.Backpropagation(zMatrixOne, hMatrixOne, prediction, groundTruthLabels, inputMatrix, weightMatrixTwo)
 
 		//Gradient Descent
 		weightMatrixOne = maths.SubtractElementsMatrices(weightMatrixOne, maths.MultiplyValueToMatrices(learningRate, partialDerivativeOne))
-		weightMatrixTwo = maths.SubtractElementsMatrices(weightMatrixOne, maths.MultiplyValueToMatrices(learningRate, partialDerivativeTwo))
 
-		//Calculate cost -- CostBCE will return a slice of means (?)
+		weightMatrixTwo = maths.SubtractElementsMatrices(weightMatrixTwo, maths.MultiplyValueToMatrices(learningRate, partialDerivativeTwo))
+
 		epochCost := maths.CostBCE(prediction, groundTruthLabels)
-		cost = append(cost, epochCost)
 
+		costs = append(costs, epochCost)
 		predictions = append(predictions, prediction)
 	}
 
+	fmt.Print(len(predictions))
+
+	fmt.Println("---")
+	fmt.Println("Training complete!")
+	fmt.Printf("Last prediction: %v \n", predictions[len(predictions)-1])
+	fmt.Printf("Expected labels: %v \n", groundTruthLabels)
+
+	data.CreatePlot(costs, iterations)
+
 	//Evaluate
+	// fmt.Println(costs)
+	// fmt.Println(predictions)
 }
